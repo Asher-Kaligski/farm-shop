f0c736b85a28e1c3d29504d099e3c28e2b0c57d2
Asher7 <ashe2803@gmail.com>
2020-09-10 12:36:26 +0300

    productQuantity component has been updated

--- src/components/productCart.jsx
+++ src/components/productCart.jsx
@@ -2,29 +2,77 @@ import React, { Component } from 'react';
 
 import ProductQuantity from './productQuantity';
 
+import shoppingCartService from './../services/shoppingCartService';
+
+import { toast } from 'react-toastify';
+
 class ProductCart extends Component {
-    state = {  };
-    render() { 
-        const {product, index} = this.props;
-        // const isBreakRow = ((index + 1) % 4 === 0) ? true : false;
-       // console.log(product);
-        return (
-            <React.Fragment>
-            <div className="card mb-4" >
-            <img class="card-img-top" src={product.imageUrl} style={{height: '40vh'}} alt=""></img>
-            
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item text-center">{product.farm.name}</li>
-              <li className="list-group-item text-center font-weight-bold">{product.title}</li>
-              <li className="list-group-item text-center">{product.price}$</li>
-            </ul>
-            <ProductQuantity product={product}/>
-            {/* <a href="#" className="btn btn-primary">Add To Cart</a> */}
-          </div>
-          {/* {isBreakRow && <div className="w-100"></div>} */}
-          </React.Fragment>
-         );
+  state = { quantity: 0 };
+
+
+  componentDidMount() {
+    this.setState({ quantity: this.props.product.quantity });
+  }
+
+
+  handleIncrement = async () => {
+
+    try {
+      //  this.setState({ selectedCategory: categoryName });
+      console.log('handleIncrement', this.props.product.quantity);
+      let quantity = this.state.quantity;
+      quantity += 1;
+
+
+      // await shoppingCartService.updateCart(items);
+
+      this.setState({ quantity });
+
+
+      toast.success('The shopping cart has been updated successfully')
+
+    } catch (err) {
+      toast.error(err);
     }
+
+
+
+
+  };
+
+
+
+  handleDecrement = async () => {
+    //  this.setState({ selectedCategory: categoryName });
+    console.log('handleDecrement', this.props.product.quantity);
+  };
+
+
+
+  render() {
+    const { product, index } = this.props;
+
+    if (product.quantity)
+      console.log(product);
+    // const isBreakRow = ((index + 1) % 4 === 0) ? true : false;
+    // console.log(product);
+    return (
+      <React.Fragment>
+        <div className="card mb-4" >
+          <img class="card-img-top" src={product.imageUrl} style={{ height: '40vh' }} alt=""></img>
+
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item text-center">{product.farm.name}</li>
+            <li className="list-group-item text-center font-weight-bold">{product.title}</li>
+            <li className="list-group-item text-center">{product.price}$</li>
+          </ul>
+          <ProductQuantity quantity={this.state.quantity} onDecrement={this.handleDecrement} onIncrement={this.handleIncrement} />
+          {/* <a href="#" className="btn btn-primary">Add To Cart</a> */}
+        </div>
+        {/* {isBreakRow && <div className="w-100"></div>} */}
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default ProductCart;
--- src/components/productQuantity.jsx
+++ src/components/productQuantity.jsx
@@ -1,31 +1,54 @@
 import React, { Component } from 'react';
 
-class ProductQuantity extends Component {
-    state = {  };
-    render() { 
-        const {product} = this.props;
-        // const isBreakRow = ((index + 1) % 4 === 0) ? true : false;
-        if (product.quantity)
-        console.log(product);
-
-        if (!product.quantity) return <a href="#" className="btn btn-primary">Add To Cart</a>;
-        return (
-            <React.Fragment>
-            
-            <div className="row no-gutters">
-                <div className="col-2">
-                <button className="btn btn-secondary  btn-block">-</button>
-                </div>
-                <div className="col text-center">{product.quantity}</div>
-                <div className="col-2">
-                    <button className="btn btn-secondary btn-block">+</button>
-                </div>
-            </div>
+const ProductQuantity = ({quantity,  onIncrement, onDecrement}) => {
+    
+    if (!quantity) return <button  className="btn btn-primary" onClick={onIncrement}>Add To Cart</button>;
+    return (
+        <React.Fragment>
         
-         
-          </React.Fragment>
-         );
-    }
+        <div className="row no-gutters">
+            <div className="col-2">
+            <button className="btn btn-secondary  btn-block" onClick={onDecrement}>-</button>
+            </div>
+            <div className="col text-center">{quantity}</div>
+            <div className="col-2">
+                <button className="btn btn-secondary btn-block" onClick={onIncrement}>+</button>
+            </div>
+        </div>
+    
+     
+      </React.Fragment>
+     );
 }
  
 export default ProductQuantity;
+
+// class ProductQuantity extends Component {
+//     state = {  };
+//     render() { 
+//         const {product} = this.props;
+//         // const isBreakRow = ((index + 1) % 4 === 0) ? true : false;
+//         if (product.quantity)
+//         console.log(product);
+
+//         if (!product.quantity) return <a href="#" className="btn btn-primary">Add To Cart</a>;
+//         return (
+//             <React.Fragment>
+            
+//             <div className="row no-gutters">
+//                 <div className="col-2">
+//                 <button className="btn btn-secondary  btn-block">-</button>
+//                 </div>
+//                 <div className="col text-center">{product.quantity}</div>
+//                 <div className="col-2">
+//                     <button className="btn btn-secondary btn-block">+</button>
+//                 </div>
+//             </div>
+        
+         
+//           </React.Fragment>
+//          );
+//     }
+// }
+ 
+//export default ProductQuantity;
